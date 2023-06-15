20684f0cdce9b9eb30149813455f664dbae5d5af
Kamesas <semakaleksandr2014@gmail.com>
2023-06-15 11:06:51 +0300

    add blog example

---
+++ src/app/blog/[id]/page.tsx
@@ -0,0 +1,20 @@
+async function getData(props: any) {
+  const id = props?.params?.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`);
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function BlogItem(props: any) {
+  const data = await getData(props);
+  return (
+    <main className="min-h-screen">
+      <h1>Blog Item</h1>
+      <div>id: {data.id}</div>
+      <div>{data?.title}</div>
+    </main>
+  );
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+async function getData() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Blog() {
+  const data = await getData();
+
+  return (
+    <main className="min-h-screen">
+      <h1>Blog page</h1>
+
+      <div>Posts</div>
+
+      <ul>
+        {data.map((item: any) => {
+          return (
+            <li key={item.id}>
+              <Link href={`/blog/${item.id}`}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
+}
