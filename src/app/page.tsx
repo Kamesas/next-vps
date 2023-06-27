1498d8d2342bb30e7900316d6a0dc3da316152c9
Kamesas <semakaleksandr2014@gmail.com>
2023-06-27 08:26:46 +0300

    increase next version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,8 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <h1>Home page </h1>
-      <div>Changed next version</div>
       <div className="text-lg uppercase text-center text-red-600 font-bold">
-        from "next": "13.4.5", to "next": "13.2.4",
+        deploy test
       </div>
     </main>
   );
