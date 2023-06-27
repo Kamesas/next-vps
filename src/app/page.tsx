751ca4aee7645dd6ed52ebbfb88ed83364065616
Kamesas <semakaleksandr2014@gmail.com>
2023-06-27 07:35:27 +0300

    three

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="min-h-screen">
       <h1>Home page </h1>
       <div className="text-lg uppercase text-center text-red-600 font-bold">
-        Next 2. Is it works - two
+        Next 2. Is it works - three
       </div>
     </main>
   );
