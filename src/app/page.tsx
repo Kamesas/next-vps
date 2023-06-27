9c3f3d5c03f2fc92c5cc84d37d55dc8ea5c5aa5b
Kamesas <semakaleksandr2014@gmail.com>
2023-06-27 08:23:07 +0300

    change next version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <h1>Home page </h1>
+      <div>Changed next version</div>
       <div className="text-lg uppercase text-center text-red-600 font-bold">
-        Next 2. Is it works - three
+        from "next": "13.4.5", to "next": "13.2.4",
       </div>
     </main>
   );
